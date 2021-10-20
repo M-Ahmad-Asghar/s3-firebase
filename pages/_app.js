b998c03d88f3dacbd650bf2a19bb90094c748398
Ahmad Asghar <ahmadasg2366@gmail.com>
2021-10-20 16:03:16 +0500

    error resolved

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import { Provider } from 'react-redux'
 import { createWrapper } from 'next-redux-wrapper'
-import store from '../config/store'
+import store from '../config/Store'
 import Header from '../components/header/Header'
 
 function MyApp({ Component, pageProps }) {
