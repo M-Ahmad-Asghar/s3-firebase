0ffd7066fe3f046c221c691af32632bbe984e006
Ahmad Asghar <ahmadasg2366@gmail.com>
2021-10-23 19:19:02 +0500

    name changed

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react"
 import { useRouter } from "next/router"
-export default function index() {
+export default function Index() {
   const router = useRouter()
    useEffect(() => {
     router.push('login')
