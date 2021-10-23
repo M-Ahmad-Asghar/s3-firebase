8a14f03484b4a66c08d7b1698e71243f6fe6e709
Ahmad Asghar <ahmadasg2366@gmail.com>
2021-10-23 16:53:32 +0500

    final changes added

--- components/header/Header.js
+++ components/header/Header.js
@@ -7,7 +7,7 @@ import { logout, authStateChk } from '../../store/actions/AuthAction'
 import { useRouter } from "next/router";
 
 
-const Header = () => {
+const Header = ({user}) => {
     const dispatch = useDispatch();
     const [pending, setPending] = useState(true)
     const Router = useRouter()
